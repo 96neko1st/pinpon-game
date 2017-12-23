ad2fd012321abc54b2142e522790e904a87e4d71
96neko1st <seed.kira07@gmail.com>
2017-12-23 21:52:39 +0900

    add:機能追加 RESTART機能を追加。

--- www/js/main.js
+++ www/js/main.js
@@ -5,7 +5,9 @@ $(function(){
         myPaddle,
         mouseX,
         score = 0,
-        scoreLabel;
+        scoreLabel,
+        isPlaying = false,
+        timerId;
     
     if(!canvas || !canvas.getContext) return false;
     ctx = canvas.getContext("2d");
@@ -29,9 +31,9 @@ $(function(){
         this.x = x;
         this.y = y;
         this.draw = function(text){
-            ctx.font = 'bold 14px "Century Gothic"';
+            ctx.font = 'bold 64px "Century Gothic"';
             ctx.fillStyle = "#00AAFF";
-            ctx.textAlign = "left";
+            ctx.textAlign = "center";
             ctx.fillText(text, this.x, this.y);
         }
     }
@@ -63,18 +65,38 @@ $(function(){
             }
             //下
             if(this.y + this.r > canvas.height){
-                console.log("game over");
+                isPlaying = false;
+                $("#btn").text("REPLAY?").fadeIn();
             }
         };
         this.checkCollision = function(paddle){
             if((this.y + this.r > paddle.y && this.y + this.r < paddle.y + paddle.h) &&
             (this.x > paddle.x - paddle.w / 2 && this.x < paddle.x + paddle.w /2)){
                 score++;
+                if(score % 3 === 0){
+                    this.vx *=1.2;
+                    paddle.w *=0.8;
+                }
                 this.vy *=-1;
             }
         }
     };
     
+    function rand(min, max){
+        return Math.floor(Math.random() * (max - min+1)) + min;
+    }
+    
+    //初期化
+    function init(){
+        isPlaying = true;
+        //パドルの生成
+        myPaddle = new Paddle(100, 10);   
+        //ボールの生成
+        myBall = new Ball(rand(50,250), rand(10, 80), rand(3, 8), rand(3, 8), 6);
+        //scoreのラベルの生成
+        scoreLabel = new Label(canvas.width/2, canvas.height /2 + 30);
+        scoreLabel.draw(score);
+    }
     
     //ステージを背景色にしてクリア
     function clearStage(){
@@ -85,26 +107,23 @@ $(function(){
     //Stageのアップデート
     function update(){
         clearStage();
-        scoreLabel.draw("SCORE: " + score);
+        scoreLabel.draw(score);
         myPaddle.draw();
         myPaddle.move();
         myBall.draw();
         myBall.move();
         myBall.checkCollision(myPaddle);
-        setTimeout(function(){
+        timerId = setTimeout(function(){
             update();
         },30);
+        if(!isPlaying) clearTimeout(timerId);
     }
     
-    //パドルの生成
-    myPaddle = new Paddle(100, 10);   
-    //ボールの生成
-    myBall = new Ball(100, 100, 5, 5, 6);
-    //scoreのラベルの生成
-    scoreLabel = new Label(10, 25);
-    scoreLabel.draw("SCORE: " + score);
-    
-    update();
+    $("#btn").click(function(){
+        $(this).fadeOut();
+        init();
+        update();
+    })
     
     //マウスのx座標を取得
     $("body").mousemove(function(e){
