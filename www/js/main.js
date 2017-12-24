570801356813286b8bc1509371e88907c5551b43
96neko1st <seed.kira07@gmail.com>
2017-12-24 22:06:57 +0900

    upgrade ボールに残像を追加。

--- www/js/main.js
+++ www/js/main.js
@@ -1,4 +1,6 @@
 $(function() {
+    "use strict";
+    
     var canvas = document.getElementById("mycanvas");
     var ctx,
         myBall,
@@ -8,14 +10,19 @@ $(function() {
         scoreLabel,
         isPlaying = false,
         timerId,
-        audioReferect;
+        audioReferect,
+        img;
+        
     if (!canvas || !canvas.getContext) return false;
     ctx = canvas.getContext("2d");
+    img = new Image();
+    img.src = "./img/f005.png";
     audioReferect = new Audio("./audio/cursor7.mp3");
+    
     //パドルクラス
     var Paddle = function(w, h) {
         this.x = canvas.width / 2;
-        this.y = canvas.height - 80;
+        this.y = canvas.height - 100;
         this.w = w;
         this.h = h;
         this.draw = function() {
@@ -67,7 +74,7 @@ $(function() {
             if (this.y + this.r > canvas.height) {
                 isPlaying = false;
                 $("#btn")
-                    .text("REPLAY?")
+                    .text("REPLAY")
                     .fadeIn();
             }
         };
@@ -76,6 +83,7 @@ $(function() {
                 if (this.x > paddle.x - paddle.w / 2 && this.x < paddle.x + paddle.w / 2) {
                     audioReferect.play();
                     score++;
+                    this.vy *= -1;
                     if (score % 3 === 0) {
                         this.vx *= 1.2;
                         paddle.w *= 0.9;
@@ -84,14 +92,10 @@ $(function() {
                         if (this.vx > 0) {
                             this.vx *= -1;
                         }
-                        this.vy = this.vy * -1;
                     } else if (this.x > paddle.x + paddle.w / 2 * 1 / 3) {
                         if (this.vx < 0) {
                             this.vx *= -1;
                         }
-                        this.vy *= -1;
-                    } else {
-                        this.vy *= -1;
                     }
                 }
             }
@@ -99,16 +103,12 @@ $(function() {
     };
     //初期化
     function init() {
-        canvas.width = $(document)
-            .width() - 10;
-        canvas.height = $(document)
-            .height() - 10;
         score = 0;
         isPlaying = true;
         //パドルの生成
         myPaddle = new Paddle(250, 30);
         //ボールの生成
-        myBall = new Ball(100, 100, 11, 11, 36);
+        myBall = new Ball(100, 100, 15, 15, 36);
         //scoreのラベルの生成
         scoreLabel = new Label(canvas.width / 2, canvas.height / 2 + 30);
         scoreLabel.draw(score);
@@ -116,7 +116,11 @@ $(function() {
     //ステージを背景色にしてクリア
     function clearStage() {
         ctx.fillStyle = "#AAEDFF";
+        ctx.globalAlpha = 0.6;
         ctx.fillRect(0, 0, canvas.width, canvas.height);
+        //ctx.drawImage(img,0,0,canvas.width,canvas.height);
+        //ctx.drawImage(img, 0, canvas.height - img.height * 0.8, img.width*0.8, img.height*0.8);
+        ctx.globalAlpha = 1;
     }
     //Stageのアップデート
     function update() {
@@ -154,3 +158,9 @@ $(function() {
         mouseX = e.touches[0].pageX;
     });
 });
+
+
+$(document).ready(function(){
+    $("#mycanvas").get(0).width = $(document).width();
+    $("#mycanvas").get(0).height = $(document).height() - 10;
+})
